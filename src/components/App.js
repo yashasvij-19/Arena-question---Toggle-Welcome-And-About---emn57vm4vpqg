ed92cea92a5b23eab4cfd2789981326d2338b7d0
Yashasvi Vij <yashasvivij01@gmail.com>
2023-08-11 21:52:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,16 @@ const App = () => {
   const [toggle, setToggle] = React.useState(true);
 
   function handleToggle() {
-    let div1 = document.getElementById("welcome-div");
-    let div2 = document.getElementById("about-div");
-    if (toggle) {
-      div1.style.display = "block";
-      div2.style.display = "none";
-      setToggle(false);
-    } else {
-      div1.style.display = "none";
-      div2.style.display = "block";
-      setToggle(true);
+      setToggle(!toggle);
     }
   }
 
   return (
     <div id="main">
+    {toggle ?
       <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
-      </div>
+      </div> :
       <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
@@ -30,6 +22,7 @@ const App = () => {
           opportunities.
         </h1>
       </div>
+}
       <button id="toggle" onClick={handleToggle}>
         Toggle
       </button>
